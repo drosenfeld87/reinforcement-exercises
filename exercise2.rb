69beecd12b66b8c4a573939b9ca913be2b035c13
debbie <drosenfeld87@gmail.com>
2018-01-30 16:00:55 -0500

    complete exercise2

--- exercise2.rb
+++ exercise2.rb
@@ -8,8 +8,17 @@ puts "on a scale of 1 to 5 rate your appreciation for documentaries"
 puts "on a scale of 1 to 5 rate your appreciation for dramas"
   dramas_answer = gets.chomp
 puts "on a scale of 1 to 5 rate your appreciation for comedies"
+  comedies_answer = gets.chomp
 
-  if documentaries_answer = => 4
+  if documentaries_answer >= "4"
     puts "I suggest #{documentaries}"
   end
-  
+  if documentaries_answer <= "3" && comedies_answer >= "4" && drama_answer >= "4"
+    puts "I suggest #{dramedies}"
+  end
+  if dramas_answer >= "4"
+    puts "I suggest #{dramas}"
+  end
+  if comedies_answer >= "4"
+    puts "I suggest #{comedies}"
+  end
